f01e44171efa4c82a04df9683b76427a23b160e7
Luigi Serra <luigser@gmail.com>
2018-10-09 10:47:29 +0200

    config file

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  port: 8001,
+  host: 'localhost',
+  https_key: './examples/snakeoil.key',
+  https_cert: './examples/snakeoil.crt',
+  db_name: 'ethersheet',
+  db_user: 'root',
+  db_password: 'is15rdc',
+  db_host: 'localhost',
+  db_type: 'mysql',
+  debug: false,
+  default_row_count: 1,
+  default_col_count: 1,
+  expire_days: 0,
+  intro_text: "welcome to ethersheet, enter a sheet name to get started"
+}
